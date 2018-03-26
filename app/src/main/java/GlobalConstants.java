ad63143f439ced398ff32b4e15c96ff118dd2f6f
ChrisMcMahon123 <chris.mcmahon4@hotmail.com>
2018-03-25 22:27:50 -0400

    Completed all of Accounts functionality

---
+++ app/src/main/java/GlobalConstants.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Chris on 3/25/2018.
+ */
+
+public class GlobalConstants {
+}
---
+++ app/src/main/java/com/mcmah113/mcmah113expensesiq/TransactionsNewFragment.java
@@ -0,0 +1,31 @@
+package com.mcmah113.mcmah113expensesiq;
+
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class TransactionsNewFragment extends Fragment {
+
+
+    public TransactionsNewFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_transactions_new, container, false);
+    }
+
+}
