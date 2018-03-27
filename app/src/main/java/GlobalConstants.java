cb2b651c133d83235f8df105267d69b10e05528c
ChrisMcMahon123 <chris.mcmahon4@hotmail.com>
2018-03-27 01:19:21 -0400

    Initial Reports / Transactions groundwork in place for the frontend and backend

--- app/src/main/java/GlobalConstants.java
+++
@@ -1,8 +0,0 @@
-package PACKAGE_NAME;
-
-/**
- * Created by Chris on 3/25/2018.
- */
-
-public class GlobalConstants {
-}
---
+++ app/src/main/java/com/mcmah113/mcmah113expensesiq/GlobalConstants.java
@@ -0,0 +1,8 @@
+package com.mcmah113.mcmah113expensesiq;
+
+/**
+ * Created by Chris on 3/26/2018.
+ */
+
+public class GlobalConstants {
+}
